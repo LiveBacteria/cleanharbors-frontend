eec873b2f6755b550520fa27368e2effa9e718f3
Tyler <tylerpoore2000@gmail.com>
2019-10-10 23:53:16 -0700

    Implemented electron as standalone app

---
+++ main.js
@@ -0,0 +1,29 @@
+const electron = require('electron');
+const { app, BrowserWindow } = require('electron');
+const path = require("path");
+
+function createWindow () {
+    // Create the browser window.
+    let win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        frame: true,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    win.removeMenu();
+    BrowserWindow.addDevToolsExtension(`./extension/`);
+    // and load the index.html of the app.
+    win.loadURL('http://winweb.cleanharbors.com/Vehicle/');
+}
+
+app.on('ready', () => createWindow());
+
+app.on('window-all-closed', () => {
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+    app.quit()
+}
+})
